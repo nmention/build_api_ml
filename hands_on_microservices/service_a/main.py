a81492aec4cf2c6cf5da9c0f1c2d3759f7c2513c
nmention <noe.mention@protonmail.com>
2025-03-10 13:41:45 +0100

    added new elements

--- hands_on_microservices/service_a/main.py
+++ hands_on_microservices/service_a/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException, Query
+
+from pydantic import BaseModel
 import httpx
+from typing import Union
+from typing import Annotated
+import pandas as pd
+
+
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 app = FastAPI()
+
 @app.get("/call-service-b")
 async def call_service_b():
     # Define the path to the Unix Domain Socket
@@ -13,3 +25,88 @@ async def call_service_b():
     async with httpx.AsyncClient(transport=transport) as client:
         response = await client.get(url)
     return response.json()
+
+truc = "yes"
+truc2 = "yes2"
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+
+sqlite_file_name = "/app/database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/")
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
+
+
--- hands_on_microservices/service_b/main.py
+++ hands_on_microservices/service_b/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
 
 app = FastAPI()
+
+
 @app.get("/data")
 async def get_data():
     return {"message": "Hello from Service B!"}
